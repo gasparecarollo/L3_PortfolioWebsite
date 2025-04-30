66c28deabdaa791072f5c924af21920b8465bf9d
Gaspare Carollo <gasparecarollo@pursuit.org>
2025-04-30 17:05:45 -0400

    Added Portfolio.html

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './Styles/Navbar.css';
+function App() {
+
+  return (
+    <>
+      <html>
+
+        <body>
+          <nav class="navbar">
+
+            <ul class="navbar-links">
+              <li><a href="src/Pages/Home.html">|Home|</a></li>
+              <li><a href="src/Pages/Aboutme.html">|About Me|</a></li>
+              <li><a href="src/Pages/Contact.html">|Contact|</a></li>
+              <li><a href="#"></a></li>
+            </ul>
+          </nav>
+
+          <main> Welcome to my Portfolio Website</main>
+        </body>
+
+      </html>
+    </>
+  )
+}
+
+export default App
